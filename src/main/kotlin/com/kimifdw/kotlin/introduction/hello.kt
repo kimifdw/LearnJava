a7e82970e01902e0262d96698af398da713c2ae0
fudongwei <fudongwei@sanjiang365.com>
2017-01-25 15:13:49 +0800

    kotlin's helloworld

---
+++ src/main/kotlin/com/kimifdw/kotlin/introduction/hello.kt
@@ -0,0 +1,16 @@
+package com.kimifdw.kotlin.introduction
+
+/**
+ * Created by kimiyu on 2017/1/23.
+ */
+fun getHelloString(): String {
+    return "Hello,world!"
+}
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        return
+    }
+
+    println(getHelloString())
+}
