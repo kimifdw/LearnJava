39214625ab06d803160f54e77b290c1ce3d33f1c
fudongwei <fudongwei@sanjiang365.com>
2017-02-15 13:47:55 +0800

    多线程知识点（synchronzied）更新

---
+++ src/main/java/com/kimifdw/java/concurrent/JoinDemo.java
@@ -0,0 +1,31 @@
+package com.kimifdw.java.concurrent;
+
+/**
+ * join实例
+ */
+public class JoinDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("First task started");
+                System.out.println("Sleeping for 2 seconds");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                System.out.println("First task completed");
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("Second task completed");
+            }
+        });
+
+        t.start();
+        t2.start();
+        t.join(); // 等待t2线程完成后再执行t线程
+    }
+}
