30c588f1c6d21b793c8e4277d5f2188d181ccbe4
fudongwei <fudongwei@sanjiang365.com>
2017-02-19 14:29:19 +0800

    增加以委托实现线程安全的类demo

---
+++ src/main/java/com/kimifdw/java/concurrent/DelegatingVehicleTracker.java
@@ -0,0 +1,44 @@
+package com.kimifdw.java.concurrent;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
+
+
+/**
+ * 基于委托的线程安全类
+ */
+public class DelegatingVehicleTracker{
+
+    private final ConcurrentHashMap<String,Point> locations;
+    private final Map<String,Point> unmodifiableMap;
+
+    public DelegatingVehicleTracker(Map<String,Point> points){
+        locations = new ConcurrentHashMap<>(points);
+        unmodifiableMap = Collections.unmodifiableMap(locations);
+    }
+
+    public Map<String,Point> getLocations(){
+        return unmodifiableMap;
+    }
+    
+    public Point getLocation(String id){
+        return locations.get(id);
+    }
+
+    public void setLocation(String id,int x,int y){
+        if(locations.replace(id, new Point(x, y))==null){
+            throw new IllegalArgumentException("invalid vehicle name:"+id);
+        }
+    }
+
+
+}
+
+class Point{
+    public final int x,y;
+
+    public Point(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+}
