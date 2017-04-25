5af8b744dad3472871cae02eacc5e3f2085155ed
fudongwei <fudongwei@sanjiang365.com>
2017-04-25 21:13:51 +0800

    1. code cleanup 2. add netty demo

---
+++ project-netty/src/main/java/com/kimi/netty/EchoClient.java
@@ -0,0 +1,60 @@
+package com.kimi.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * netty 客户端
+ * Created by kimiyu on 2017/4/25.
+ */
+public class EchoClient {
+
+    private final String host;
+
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            // 同步关闭线程池并释放资源
+            eventLoopGroup.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Usage: " + EchoClient.class.getSimpleName() + "<host><port>");
+            return;
+        }
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        new EchoClient(host, port).start();
+
+    }
+}
---
+++ project-netty/src/main/java/com/kimi/netty/EchoServer.java
@@ -0,0 +1,57 @@
+package com.kimi.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * netty server端
+ * Created by kimiyu on 2017/4/25.
+ */
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage：" + EchoServer.class.getSimpleName() + "<port>");
+        }
+
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    public void start() throws InterruptedException {
+        final EchoServerHandler echoServerHandler = new EchoServerHandler();
+        EventLoopGroup eventExecutors = new NioEventLoopGroup();
+        try {
+            // ServerBootstrap绑定server
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(eventExecutors)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(echoServerHandler);
+                        }
+                    });
+            // 同步等待绑定完成
+            ChannelFuture channelFuture = serverBootstrap.bind().sync();
+            // 同步等待关闭完成
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            eventExecutors.shutdownGracefully().sync();
+        }
+    }
+}
