6c7dbba3f29c59f448fd26048909286d68b6dbe0
fudongwei <fudongwei@sanjiang365.com>
2018-02-23 15:35:37 +0800

    修改kafka安装脚本

--- project-hadoop/kafka-install.sh
+++ project-hadoop/kafka-install.sh
@@ -2,3 +2,8 @@
 
 wget http://mirror.bit.edu.cn/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz
 sudo tar -xzf kafka_2.11-1.0.0.tgz -C /usr/local/
+
+sudo chown -R sjgw:sjgw /usr/local/kafka_2.11-1.0.0
+sudo echo "#KAFKA VARIABLES START
+export KAFKA_HOME=/usr/local/kafka_2.11-1.0.0
+#KAFKA VARIABLES END" >> .bashrc
